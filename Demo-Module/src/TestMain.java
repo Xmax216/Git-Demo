3a371db01d6dd03d1b56ee4e2a15e2a38cf74bcc
Xmax216 <2622472355@qq.com>
2021-04-27 17:06:06 +0800

    项目分支的修改4 TestMain多加了个sout（hello master master master）

--- Demo-Module/src/TestMain.java
+++ Demo-Module/src/TestMain.java
@@ -1,5 +1,6 @@
 public class TestMain {
     public static void main(String[] args) {
         System.out.println("hello master");
+        System.out.println("hello master master master");
     }
 }
